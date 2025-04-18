9a9637c5c73eb134d1147b67ca3d296b529fc90e
hsu-git <hsu080206@gmail.com>
2025-04-18 11:50:24 +0900

    AnimalController, AnimalService, AnimalServiceImpl 생성

---
+++ src/main/java/org/example/bootrest/controller/AnimalController.java
@@ -0,0 +1,8 @@
+package org.example.bootrest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/animal")
+public class AnimalController {
+
+}
---
+++ src/main/java/org/example/bootrest/service/AnimalServiceImpl.java
@@ -0,0 +1,7 @@
+package org.example.bootrest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimalServiceImpl implements AnimalService {
+}
