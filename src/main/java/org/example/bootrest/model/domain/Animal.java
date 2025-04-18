48223b74fca3dfdeccad8b8e24057f05a49abea0
hsu-git <hsu080206@gmail.com>
2025-04-18 11:47:35 +0900

    Animal 레코드, AnimalMapper 인터페이스

---
+++ src/main/java/org/example/bootrest/model/domain/Animal.java
@@ -0,0 +1,8 @@
+package org.example.bootrest.model.domain;
+
+public record Animal(String uuid,
+                     String name,
+                     String description,
+                     String story,
+                     String createdAt) {
+}
